a618d89f9982046bb4c65d672a12e9e2fd05a431
palomerocarolina <palomero.carolina@gmail.com>
2024-07-14 22:05:32 +0200

    first part complete

--- Evaluación-módulo2.sql
+++ Evaluación-módulo2.sql
@@ -84,7 +84,6 @@ GROUP BY rating; -- para que nos agrupe todas las filas que tienen el mismo valo
 
 -- 10. Encuentra la cantidad total de películas alquiladas por cada cliente y muestra el ID del cliente, su nombre y apellido junto con la cantidad de películas alquiladas.
 
-
 SELECT customer_id, first_name, last_name, COUNT(*) AS rentals_per_customer
 FROM customer
 INNER JOIN rental
@@ -95,16 +94,23 @@ GROUP BY customer_id;
 
 -- 11. Encuentra la cantidad total de películas alquiladas por categoría y muestra el nombre de la categoría junto con el recuento de alquileres.
 
+-- SELECT*FROM sakila.category -- ahora quiero llegar hasta el rental_id para hacer el recuento
 
-
-
+SELECT name AS Categoria, COUNT(r.rental_id) AS Total_peliculas_alquiladas
+FROM category c 
+	INNER JOIN film_category fc USING (category_id)
+	INNER JOIN film f USING (film_id)
+	INNER JOIN inventory i USING (film_id)
+	INNER JOIN rental r USING (inventory_id)
+GROUP BY Categoria;
 
 
 
 -- 12. Encuentra el promedio de duración de las películas para cada clasificación de la tabla film y muestra la clasificación junto con el promedio de duración.
 
-
-
+SELECT AVG(`length`), rating
+FROM film
+GROUP BY rating;
 
 
 
@@ -120,68 +126,157 @@ WHERE title = "Indian Love";
 
 -- 14. Muestra el título de todas las películas que contengan la palabra "dog" o "cat" en su descripción.
 
-
+SELECT title
+FROM film
+WHERE description LIKE "%dog%" OR description LIKE "%cat%";
 
 
 
 -- 15. Hay algún actor o actriz que no apareca en ninguna película en la tabla film_actor.
 
+SELECT first_name, last_name
+FROM actor
+WHERE actor_id NOT IN (
+	SELECT actor_id 
+	FROM film_actor);
 
+	-- otra forma de hacerlo
+	SELECT first_name, last_name
+	FROM actor
+	INNER JOIN film_actor USING (actor_id)
+	WHERE actor_id IS NULL;
 
 
 
 -- 16. Encuentra el título de todas las películas que fueron lanzadas entre el año 2005 y 2010.
 
-
-
-
-
+SELECT title
+FROM film 
+WHERE release_year BETWEEN "2005" AND "2010";
 
 
 
 -- 17. Encuentra el título de todas las películas que son de la misma categoría que "Family".
 
+SELECT title
+FROM film
+	INNER JOIN film_category USING(film_id)
+WHERE category_id = 8;
+
+	-- consulto las tablas con las que se relaciona la tabla film para ver de dónde puedo sacar la categoría.
+	-- SELECT*FROM sakila.film_category;
+	-- SELECT*FROM sakila.category -- veo que la categoría Family tiene el id 8
+
+
+
+-- 18. Muestra el nombre y apellido de los actores que aparecen en más de 10 películas.
+
+SELECT first_name, last_name
+FROM actor 
+WHERE actor_id IN (SELECT actor_id 
+	FROM film_actor
+	GROUP BY actor_id 
+	HAVING COUNT(film_id) > 10);
+
+	-- hago varias consultas entre las tablas en común para ver cómo lo puedo relacionar
+
+		-- SELECT * FROM sakila.film_actor
+	 
+		-- SELECT actor_id 
+		-- FROM film_actor
+		-- GROUP BY actor_id 
+		-- HAVING COUNT(film_id) > 10
 
 
 
 
+-- 19. Encuentra el título de todas las películas que son "R" y tienen una duración mayor a 2 horas en la tabla film.
+
+SELECT title
+FROM film 
+WHERE rating = "R" AND `length` > 120;
 
 
 
+-- 20. Encuentra las categorías de películas que tienen un promedio de duración superior a 120 minutos y muestra el nombre de la categoría junto con el promedio de duración.
+
+SELECT AVG(length), category_id
+FROM film 
+	INNER JOIN film_category USING (film_id)
+	GROUP BY category_id
+	HAVING AVG(length) > 120
+	
+	-- como nos piden la categoría, tengo que ir a la tabla de film_category que se relacionan con el film_id
+
+	-- SELECT category_id
+	-- FROM sakila.film_category
+	-- GROUP BY category_id
 
 
 
+-- 21. Encuentra los actores que han actuado en al menos 5 películas y muestra el nombre del actor junto con la cantidad de películas en las que han actuado.
+
+SELECT first_name, last_name, COUNT(film_id) AS NºPeliculas 
+FROM actor 
+INNER JOIN film_actor USING (actor_id)
+GROUP BY actor_id 
+HAVING COUNT(film_id) >= 5;
 
 
 
+-- 22. Encuentra el título de todas las películas que fueron alquiladas por más de 5 días. Utiliza una subconsulta para encontrar los rental_ids 
+--	con una duración superior a 5 días y luego selecciona las películas correspondientes.
+
+
+SELECT title, rental_duration
+FROM film
+WHERE rental_duration > 5
+
+
+-- Para llegar a los rental_id tenemos que pasar por la tabla inventory
+
+
+SELECT title, rental_duration, r.rental_id
+FROM film
+	INNER JOIN inventory i USING (film_id)
+	INNER JOIN rental AS r USING (inventory_id)
+WHERE r.rental_id IN (
+	SELECT rental_id 
+   	FROM rental
+   	WHERE rental_duration > 5)
+    
+--  Otra forma de encontrar la fecha es:  DATEDIFF(return_date, rental_date) > 5);
 
 
 
+-- 23. Encuentra el nombre y apellido de los actores que no han actuado en ninguna película de la categoría "Horror". Utiliza una subconsulta para encontrar 
+-- los actores que han actuado en películas de la categoría "Horror" y luego exclúyelos de la lista de actores.
+
+   
+
+	SELECT first_name, last_name
+	FROM actor 
+		
+	-- Ahora tenemos que llegar a la categoría
+	SELECT actor_id
+	    FROM film_actor 
+	    INNER JOIN film USING (film_id)
+	    INNER JOIN film_category USING (film_id)
+	    INNER JOIN category USING (category_id)
+		WHERE name = "Horror";
 
 
+-- juntamos todo haciendo una subconsulta
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+SELECT first_name, last_name
+FROM actor 
+WHERE actor_id NOT IN (
+SELECT actor_id
+    FROM film_actor 
+    INNER JOIN film USING (film_id)
+    INNER JOIN film_category USING (film_id)
+    INNER JOIN category USING (category_id)
+	WHERE name = "Horror");
 
 
 
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-##Final Evaluation Module 2
+Final Evaluation Module 2 SQL
 
 For this exercise we will use the Sakila database that we have been using during the SQL review. It is an example database that simulates a movie rental store. It contains tables such as film, actor, customer, rental, category, among others. These tables contain information about movies, actors, customers, rentals, and more, and are used to perform queries and data analysis in the context of a movie rental store.
+
+
+Objective of the evaluation
+
+The objective of this evaluation is to apply the SQL knowledge acquired during the course to perform queries and analysis in the Sakila database. 
+
+The way I approach these exercises is not only by applying all the knowledge learned in class, but also by trying to do it as simply and efficiently as possible.
